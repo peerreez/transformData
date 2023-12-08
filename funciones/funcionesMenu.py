aedfc820ede89eb4a8271c49997a1991ae55f2ea
peerrrez <dperezsx@gmail.com>
2023-12-08 11:10:51 +0100

    add menu

---
+++ funciones/funcionesMenu.py
@@ -0,0 +1,14 @@
+def mostrar_menu():
+    print("1. Opción 1")
+    print("2. Opción 2")
+    print("3. Opción 3")
+    print("4. Salir")
+
+def opcion1():
+    print("Has seleccionado la Opción 1.")
+
+def opcion2():
+    print("Has seleccionado la Opción 2.")
+
+def opcion3():
+    print("Has seleccionado la Opción 3.")
